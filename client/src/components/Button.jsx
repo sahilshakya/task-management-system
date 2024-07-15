87071564d6a64632dcc14b1c2f7ea63eb561be09
sahil shakya <sahilshakya42@gmail.com>
2024-07-15 08:34:47 +0545

    first commit

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ icon, className, label, type, onClick = () => {} }) => {
+  return (
+    <button
+      type={type || "button"}
+      className={(clsx("px-3 py-2 outline-none"), className)}
+    >
+      <span>{label}</span>
+      {icon && icon}
+    </button>
+  );
+};
+export default Button;
