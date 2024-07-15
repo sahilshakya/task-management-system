3d3e833315e846d4034ce430875cf883a29db7b8
sahil shakya <sahilshakya42@gmail.com>
2024-07-15 08:46:22 +0545

    text changes for priority charts

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -207,9 +207,7 @@ const Dashboard = () => {
       </div>
 
       <div className="w-full bg-white my-16 p-4 rounded shadow-sm">
-        <h4 className="text-xl text-gray-600 font-semibold">
-          Chart by Priority
-        </h4>
+        <h4 className="text-xl text-gray-600 font-semibold">Priority Chart</h4>
         <Chart />
       </div>
 
